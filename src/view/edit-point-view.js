6997b80bb3b5ad353a9152071fa13607037aa731
Anastasia Migaeva <pandora1983@yandex.ru>
2023-01-24 12:49:50 +0300

    вернула 1 к айдишнику

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -156,14 +156,14 @@ const createEditPointTemplate = (point, offers, pointDestinations, isPointEdit) 
         </div>
 
         <div class="event__field-group  event__field-group--time">
-          <label class="visually-hidden" for="event-start-time">From</label>
+          <label class="visually-hidden" for="event-start-time-1">From</label>
           <input class="event__input  event__input--time"
                  id="event-start-time-1"
                  type="text"
                  name="event-start-time-1"
                  value="${he.encode(tripDateFrom)}">
           &mdash;
-          <label class="visually-hidden" for="event-end-time">To</label>
+          <label class="visually-hidden" for="event-end-time-1">To</label>
           <input class="event__input  event__input--time"
                  id="event-end-time-1"
                  type="text"
